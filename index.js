bc1cd943cbf6d9ddccc368305f781e829183dd8c
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2023-11-12 19:45:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const CronJob = require('cron').CronJob;
 
 function delay(ms) {return new Promise(res => setTimeout(res, ms));}
 
-const Wallet = "TRrhyn55AtGEjgaLpj9sTbRGhqzVJ8ueNs" //wallet or contract to monitorice on Tron Network
-
 const TRONGRID_API = "https://api.trongrid.io";
 
 var tronWeb = new TronWeb({
@@ -18,44 +16,62 @@ var tronWeb = new TronWeb({
 	
 });
 
-var inicio = new CronJob('0 */2 * * * *', async() => {
+var inicio = new CronJob('0 */1 * * * *', async() => {
 	console.log('-----------------------------------');
 	console.log('>Running :'+new Date().toLocaleString());
 	console.log('-----------------------------------');
 
-	await watchWallet()
-	console.log('=>Done: '+new Date().toLocaleString());
+	watchWallet("TRrhyn55AtGEjgaLpj9sTbRGhqzVJ8ueNs", "energy", 200000, "1d", 200000, process.env.token_EBOT, "1598897599");
+	watchWallet("TW874JtRJDSwincEm64s2WbSQsMBUsiP1L", "band", 1000, "3d", 284, process.env.token_EBOT_2, "2129154100");
+
 	
 });
 
 inicio.start();
 
-async function watchWallet(){
-    var recursos = await tronWeb.trx.getAccountResources(Wallet)
+async function watchWallet(viewWallet, resource, amount, time, valorMonitoreo, token, id){
+    let recursos = await tronWeb.trx.getAccountResources(viewWallet)
 
-    var energia = recursos.EnergyLimit-recursos.EnergyUsed
-    console.log(energia) // aleta a los 100K compra 200K por 1 dia
+    var eval = 0;
+    if(resource === "energy"){
+        eval = recursos.EnergyLimit
+        if(recursos.EnergyUsed){
+            eval = recursos.EnergyLimit-recursos.EnergyUsed
+        }
+        
+    }else{
+        eval = (recursos.freeNetLimit+recursos.NetLimit)-(recursos.freeNetUsed+recursos.NetUsed)
 
-    if(energia <= 200000){
-        console.log("alquilar energia")
+    }
 
-        var result = await fetch("https://e-bot.brutusservices.com/main/energy",{
+    console.log("monitoring "+resource+": "+(eval).toLocaleString("en-us")+" "+viewWallet)
+
+    if(eval <= parseInt(valorMonitoreo) && true){
+        
+        var result = await fetch("https://e-bot.brutusservices.com/main/"+resource,{
 
             headers:{
-                "token-api": "GF1mrydEHWCV6XisHj2Y",
+                "token-api": token,
                 "Content-Type": "application/json",
             },
             method: "POST",
             body:JSON.stringify({
-                "id_api": "1598897599",
-                "wallet": Wallet,
-                "amount": 200000,
-                "time": "1",
+                "id_api": id,
+                "wallet": viewWallet,
+                "amount": amount,
+                "time": time,
                 "user_id": "string"
             })
         })
 
-        console.log(await result.json())
+        result = await result.json()
+
+        if(result.response === 1){
+            console.log("+"+ amount +" "+resource+" para:"+viewWallet)
+        }else{
+            console.log("FALLO: "+ amount +" "+resource+" para:"+viewWallet)
+        }
+
     }
 
 }
